873233c12d9ace080c690db0b9d5a632733a8252
Lindsey Carriere <lindsey.carriere@guest-tek.com>
2023-01-08 21:42:18 -0700

    Completed Section 3

---
+++ src/main/java/guru/springframework/Expression.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+public interface Expression {
+
+    Money reduce(Bank bank, String to);
+
+    Expression times(int multiplier);
+
+    Expression plus(Expression addend);
+}
